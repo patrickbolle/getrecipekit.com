8aca481b4937355b3909ab891a15f455cdbc9d07
Matt Ruetz <matt.b.ruetz@gmail.com>
2025-01-28 15:44:43 -0800

    re-added tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+export default {
+  prefix: 'twcss-',
+  content: [
+    './layout/*.liquid',
+    './templates/*.liquid',
+    './templates/customers/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+  ],
+  theme: {
+    screens: {
+      sm: '320px',
+      md: '750px',
+      lg: '990px',
+      xlg: '1440px',
+      x2lg: '1920px',
+      pageMaxWidth: '1440px',
+    },
+    extend: {
+      fontFamily: {
+        heading: 'var(--font-heading-family)',
+      },
+    },
+  },
+  plugins: [
+    await import('@tailwindcss/typography')
+  ],
+};
